38614a5dddd647bb1f7fa6fe5b708fda7ae96342
josenguerra <josenguerra@hotmail.com>
2016-05-16 16:39:52 +0100

    atualizacao
    atualizacao

--- WindowsFormsApplication1/FormPrincipal.Designer.cs
+++ WindowsFormsApplication1/FormPrincipal.Designer.cs
@@ -79,7 +79,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "ZE  GUERRA";
+            this.textBox1.Text = "Diogo MArtins";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // FormPrincipal
