92c5c8b8cd51ee6b38cd744f150a6ddd9f7c10f1
cavadinha <cavadinhapower@gmail.com>
2016-05-26 12:50:13 +0100

    26/05/inie
    utente com SNSID

--- WebService/Modelo.Designer.cs
+++ WebService/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Zé-PC\Desktop\EinsteinHospitalAplication\WebService\Modelo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\DiogoMartins\Desktop\Esgps\EinsteinHospitalAplication\WebService\Modelo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
